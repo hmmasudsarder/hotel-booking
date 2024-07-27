92a52c21cd654193781a8e5442799d467718a0d6
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-07-27 23:51:37 +0600

    navbar added complated

--- src/components/layoutProvider/LayoutProvider.jsx
+++ src/components/layoutProvider/LayoutProvider.jsx
@@ -1,12 +1,16 @@
 "use client"
 
 import { usePathname } from "next/navigation";
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 
-const LayoutProvider = () => {
+const LayoutProvider = ({children}) => {
     const pathname = usePathname();
     return (
         <div>
-            
+            {pathname !== "/login" && pathname !== "/signup" && <Navbar/>}
+            {children}
+            {pathname !== "/login" && pathname !== "/signup" && <Footer/>}
         </div>
     );
 };
